49d25f0fd6f8fd53c8c0e173063fd36a42e10b0d
tehKruser <krusej@oregonstate.edu>
2017-11-03 16:26:20 -0600

    POST and GET complete

--- anagrams.py
+++ anagrams.py
@@ -1,38 +1,68 @@
 #!/usr/bin/env python3
 
-import redis
-import json
-import ast
 from flask import Flask
 from flask import jsonify
 from flask import request
+import redis
+import json
+
 
 app = Flask(__name__)
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-r.sadd('ph', *set([7,2,0,4,3,8,1,4,4,2]))
-
 def keyWord(word):
 	return ''.join(sorted(word)).lower()
 
-@app.route("/words.<format>", methods=['POST'])
-def words(format=None):
+@app.route("/words.<data_format>", methods=['POST', 'DELETE'])
+def words(data_format=None):
 	if request.method == 'POST':
-		data_str = str(request.form.to_dict())
-		data_str = data_str[2:-7]
-		# to dict
-		data_dict = json.loads(data_str)
-		# to json
-		data = jsonify(data_dict)
-		# returns json object
-		return data, 201
+		if data_format == 'json':
+			try:
+				data = request.get_json(force=True)
+			except:
+				# return 400: content type of entity not understood
+				return "400 Bad Request: Check syntax of JSON data\n", 400
+			
+			try:
+				words = data['words']
+			except:
+				# return 422: content type understood, missing entity key
+				return '422 Unprocessable Entity: Missing "words" entity\n', 422
+			
+			for word in words:
+				r.sadd(keyWord(word), word)
+			return '', 201
+		
+		else:
+			return '400 Bad Request : JSON data not specified\n', 400
 
-@app.route("/anagrams/<word>.<format>", methods=['GET'])
-def word(word=None, format=None):
+
+@app.route("/anagrams/<word>.<data_format>", methods=['GET'])
+def word(word=None, data_format=None):
 	if request.method == 'GET':
-		anagrams = list(r.smembers(keyWord(word=word)))
-		return jsonify({'anagrams' : anagrams}), 200
+		if data_format == 'json':
+			count = len(list(r.smembers(keyWord(word))))
+			try:
+				limit = int(request.args.get('limit'))
+				anagrams = list(r.srandmember(keyWord(word), limit))
+				# if limiting to 1 and more than 1 anagram exists- don't return self
+				if limit == 1 and count > 1:
+					# Must have at least one anagram returned if it exists
+					while word == anagrams[0]:
+						anagrams = list(r.srandmember(keyWord(word), limit))
+			except:
+				anagrams = list(r.smembers(keyWord(word)))
+			
+			try:
+				anagrams.remove(word)
+			except:
+				pass
+
+			return jsonify({'anagrams' : anagrams}), 200
+
+		else:
+			return '400 Bad Request : JSON data not specified\n', 400
 
 
 
